b385b9fa044fc9b798cc9f522584c19cbcd9a7ef
AIwinski <artur.iwinski@gmail.com>
2018-04-22 20:16:35 +0200

    deleted secret info

--- config/mailer.js
+++ config/mailer.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MAILGUN_USER: 'postmaster@sandboxe9272ba18e99446d80290b803d4d5c24.mailgun.org',
-  MAILGUN_PASS: '63b03a5145c8055127c19062094a9310-db1f97ba-f0d56663'
+  MAILGUN_USER: 
+  MAILGUN_PASS: 
 };
